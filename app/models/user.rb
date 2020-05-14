e5be75d6c1378a09d7391b30d96873d2eb5e8c49
Mathew <mathewkeene22@gmail.com>
2020-05-14 14:27:17 -0700

    rm bad association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :nominee, class_name: 'Vote', foreign_key: 'nominee_id'
+  # belongs_to :nominee, class_name: 'Vote', foreign_key: 'nominee_id'
 end

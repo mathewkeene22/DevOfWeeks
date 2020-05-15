0897caae8bc29ef81e81e997b9efd9a3e2ef3c0b
Nathan Brakken <nathan.brakken@evanta.com>
2020-05-15 12:57:16 -0700

    add docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+  # FOR DOCKER
+  config.logger = Logger.new('/proc/1/fd/1')
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 

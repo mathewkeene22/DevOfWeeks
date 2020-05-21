96460e6b8042f0fc7878f9c5fc4aa1ad49ad3e1f
Mathew Keene <mathewkeene22@gmail.com>
2020-05-21 09:58:46 -0700

    better organization in election_form component

--- app/javascript/components/elections/election_form.js
+++ app/javascript/components/elections/election_form.js
@@ -1,63 +1,100 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const WriteInInput = () => {
+  const [writeIn, setWriteIn] = React.useState('');
+  const writeInOnChange = (e) => {
+    setWriteIn(e.target.value);
+  };
+  return(
+    <div className="write-in">
+      <input
+        className="write-in-input"
+        placeholder="Write in..."
+        type="text"
+        name="name"
+        onChange={writeInOnChange}
+        value={writeIn}
+      />
+    </div>
+  )
+}
+
 const ElectionForm = (props) => {
-  let [showWriteInInput, setShowWriteInInput] = React.useState(false);
-  let onChange = (e) => {
-    if(e.currentTarget.value == 'Write in your own answer!'){
-      setShowWriteInInput(true)
+  const [nomineeId, setNomineeId] = React.useState('')
+  const [showWriteInInput, setShowWriteInInput] = React.useState(false);
+  const [message, setMessage] = React.useState('');
+
+  const messageOnChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const nomineeDropdownChange = (e) => {
+    if (e.currentTarget.value == 'write_in') {
+      setShowWriteInInput(true);
+      setNomineeId(null);
     } else {
       setShowWriteInInput(false);
+      setNomineeId(e.currentTarget.value);
+      setWriteIn('');
     }
-  }
-  let nomineeDropdown = props.nominees.map((nominee) => {
-    return <option key={nominee.id}>{nominee.username}</option>;
-  });
-  let handleSubmit = () => {
-    $(".vote-message").val("");
+  };
+
+  const handleSubmit = () => {
     $.ajax({
       url: props.post_url,
-      type: "POST",
+      type: 'POST',
       data: {
         vote: {
           election_id: props.election_id,
-          nominee_id: 1,
-          // write_in_name: 'bar',
-          message: 'matt stinks',
-        }
+          nominee_id: nomineeId,
+          write_in_name: writeIn,
+          message: message,
+        },
       },
       success: function (data) {
-        alert('working')
+        alert('working');
+        $('.vote-message').val('');
       },
       error: function (data) {
-        console.log("something went wrong");
+        console.log('something went wrong');
       },
-    })
+    });
   }
 
+  const nomineeDropdown = props.nominees.map((nominee) => {
+    return (
+      <option key={nominee.id} value={nominee.id}>
+        {nominee.username}
+      </option>
+    );
+  });
+
   return (
-    <div className='election-form'>
-      <input type="hidden" className='election-id' value={props.election_id}></input>
-      <select onChange={onChange}>
+    <div className="election-form">
+      <input type="hidden" className="election-id" value={props.election_id} />
+
+      <select onChange={nomineeDropdownChange}>
+        <option value="">Select...</option>
         {nomineeDropdown}
-          <option>Write in your own answer!</option>
+        <option value="write_in">Write in your own answer!</option>
       </select>
+
       {showWriteInInput ? <WriteInInput /> : null}
-      <textarea className='vote-message' placeholder='Type something witty...' rows='4'></textarea>
-      <button type="submit" className='btn' onClick={handleSubmit}>Submit</button>
+
+      <textarea
+        className="vote-message"
+        placeholder="Type something witty..."
+        rows="4"
+        value={message}
+        onChange={messageOnChange}
+      />
+      <button type="submit" className="btn" onClick={handleSubmit}>
+        Submit
+      </button>
     </div>
   );
 }
 
-const WriteInInput = () => (
-  <div className="write-in">
-    <input className="write-in-input" placeholder="Write in..."></input>
-  </div>
-);
-
-// ReactDOM.render(
-//   <ElectionForm nominees={props.nominees} />,
-//   document.querySelector(".election-form")
-// );
-
 export default ElectionForm;
+// ReactDOM.render(<ElectionForm />, $('.election-form'));

bbe0b31c0b19a8b1b7ebc04bfceb73fffc830ece
Mathew Keene <mathewkeene22@gmail.com>
2020-06-05 11:49:46 -0700

    install material js, fix nav up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,4 @@
+import "materialize-css/dist/js/materialize";
 require('@rails/ujs').start()
 require('turbolinks').start()
 require('@rails/activestorage').start()
@@ -10,3 +11,7 @@ require("chart.js");
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+
+$(document).on("turbolinks:load", function () {
+  $(".dropdown-trigger").dropdown({ hover: false });
+});
